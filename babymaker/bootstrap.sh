c78e19938e23c64b93fdc41407b6db627d604b18
Philip Chang <sgnoohc@gmail.com>
2017-10-17 15:43:45 -0700

    :pencil: I stringed all the bootstrap commands via &&

--- babymaker/bootstrap.sh
+++ babymaker/bootstrap.sh
@@ -7,24 +7,6 @@
 source root.sh
 
 # Download RooUtil packages
-git clone git@github.com:sgnoohc/rooutil.git
-cd rooutil/
-git checkout 0.0.1
-make -j 20
-cd ../
-
-# Download CORE
-git clone git@github.com:cmstas/CORE.git
-cd CORE
-git checkout CMS3_archive
-make -j 20
-cd ../
-
-# Download CORE Helper package
-git clone git@github.com:sgnoohc/COREHelper.git
-cd COREHelper/
-git checkout 0.0.1
-make -j 20
-cd ../
+git clone git@github.com:sgnoohc/rooutil.git && cd rooutil/ && git checkout 0.0.1 && make -j 20 && cd ../ && git clone git@github.com:cmstas/CORE.git && cd CORE && git checkout CMS3_archive && make -j 20 && cd ../ && git clone git@github.com:sgnoohc/COREHelper.git && cd COREHelper/ && git checkout 0.0.1 && make -j 20 && cd ../
 
 #eof

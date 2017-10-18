8ce05eba51e77bf1ac64aba26c517ab21b361f60
Philip Chang <sgnoohc@gmail.com>
2017-10-18 10:40:07 -0700

    update

--- babymaker/bootstrap.sh
+++ babymaker/bootstrap.sh
@@ -3,8 +3,10 @@
 #  .
 # ..: P. Chang, philip@physics.ucsd.edu
 
+git clone git@github.com:sgnoohc/scripts.git
+
 # Setup root
-source root.sh
+source scripts/setup.sh
 
 # Download RooUtil packages
 git clone git@github.com:sgnoohc/rooutil.git && cd rooutil/ && git checkout 0.0.1 && make -j 20 && cd ../ && git clone git@github.com:cmstas/CORE.git && cd CORE && git checkout CMS3_archive && make -j 20 && cd ../ && git clone git@github.com:sgnoohc/COREHelper.git && cd COREHelper/ && git checkout 0.0.1 && make -j 20 && cd ../

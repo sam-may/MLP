fafdf39ecdaf0681455da78ea9dce08289a9ebb0
Samuel May <sjmay@ucsd.edu>
2017-11-09 17:32:19 -0800

    Forgot filename

--- MLP_1d/throwDarts.sh
+++ MLP_1d/throwDarts.sh
@@ -7,7 +7,7 @@ for LAYER in 1 2 3 5 10; do
         for EPOCHS in 1000 2000 10000; do
             name="MLP1D_11var_"${LAYER}"layers_"${NODES}"nodes_"${EPOCHS}"epochs"
             echo $name >> dartsList.txt
-            python3 learn_1d.py 11 ${EPOCHS} ${LAYER} samTest ${NODES}
+            python3 learn_1d.py 11 ${EPOCHS} ${LAYER} $name ${NODES}
         done
     done
 done

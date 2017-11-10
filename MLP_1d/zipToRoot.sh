19d4d14e5b8140dbadac8c03c3b889822cff49c0
Samuel May <sjmay@ucsd.edu>
2017-11-10 13:56:11 -0800

    Delete ziptoroot

--- MLP_1d/zipToRoot.sh
+++
@@ -1,6 +0,0 @@
-cp checkpoints/output_MLP_$1.txt ../output_MLP.txt
-cd ../babymaker
-source scripts/setup.sh
-run.sh -c zip_mlpoutputs_to_baby.C $1.root t 200000 dummy ../../IsoML_output.root
-cd ../bdt
-python makerocComparison.py
